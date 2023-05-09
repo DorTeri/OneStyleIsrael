898c490a459e20554b18d863f039c9c148903e36
DorTeri <dortayari@gmail.com>
2023-05-09 10:43:42 +0300

    Upload to render

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,10 @@ const http = require('http').createServer(app)
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(cookieParser())
 app.use(express.json({limit: '50mb'}))
-app.use(express.static('build'))
+app.use(express.static('public'))
 
 if (process.env.PORT === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'build') ))
+    app.use(express.static(path.resolve(__dirname, 'public') ))
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://localhost:3000' , 'https://onestyleisrael.onrender.com'],
@@ -45,7 +45,7 @@ setupSocketAPI(http)
 // so when requesting http://localhost:3030/index.html/product/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
 app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 
